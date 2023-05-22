6429f1c9d93aafd6b0db6c91e3e1229912a63dee
TuanDoan14112003 <tuandoan14112003@gmail.com>
2023-05-22 23:47:55 +1000

    fix rpc

--- discord-bot/rpc.py
+++ discord-bot/rpc.py
@@ -16,15 +16,15 @@ def on_message(client, userdata, msg):
 client = mqtt.Client()
 client.on_connect = on_connect
 client.on_message = on_message
+client.username_pw_set(username=os.getenv('DISCORD_BOT_ACCESS_KEY'))
+client.connect(os.getenv('MQTT_HOST'), 1883, 60)
+client.loop_start()
 
-
-def openGate():
-    print("vao open gate")
-    client.username_pw_set(username=os.getenv('GATE_ACCESS_KEY'))
-    client.connect(os.getenv('MQTT_HOST'), 1883, 60)
+def sendMessage(method,id):
+    print("Fire message")
     request = {
-                    "method": "OpenGate",
+                    "method": method,
                     "params": {}
             }
-    client.publish("v1/devices/me/rpc/request/1",json.dumps(request))
-    client.loop_start()
+    client.publish("v1/devices/me/rpc/request/"+str(id),json.dumps(request))
+    
